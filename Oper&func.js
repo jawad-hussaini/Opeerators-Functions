4fa80769e97ca2e6cba5e6a85bfec889fc0c445b
Syed Jawad <jawadhussaini1804@gmail.com>
2024-05-17 18:11:29 +0500

    Oper_Func

--- Oper&func.js
+++ Oper&func.js
@@ -24,74 +24,81 @@ let ride = function(check){
 ride("aliya");
 
 //                   //***** BASIC OPERATORS *****//
-// function positive(a){
-//     console.log(a)
-//     console.log(a++);
-//     console.log(a);
-//     console.log(++a);
-//     console.log(a)
-//     console.log("Done")
-// }
-// positive(1)
+function positive(a){
+    console.log(a)
+    console.log(a++);
+    console.log(a);
+    console.log(++a);
+    console.log(a)
+    console.log("Done")
+}
+positive(1)
 
-// function negative(b){
-//     console.log(b);
-//     console.log(b--);
-//     console.log(b);
-//     console.log(--b);
-//     console.log(b)
-//     console.log("Done")
-// }
-// negative(2)
+function negative(b){
+    console.log(b);
+    console.log(b--);
+    console.log(b);
+    console.log(--b);
+    console.log(b)
+    console.log("Done")
+}
+negative(2)
 
-// function multiplication(c,d){
-//     console.log(c*d);
-//     console.log(c*6);
-//     console.log(c**d);
-//     console.log("Done")
-// }
-// multiplication(2,5);
+function multiplication(c,d){
+    console.log(c*d);
+    console.log(c*6);
+    console.log(c**d);
+    console.log("Done")
+}
+multiplication(2,5);
 
-// let division = (e,f) => {  // answer will be in point
-//     console.log(e/f); 
-//     console.log(f/e);
-//     console.log("Done")
-// }
-// division(2,30)
+let division = (e,f) => {  // answer will be in point
+    console.log(e/f); 
+    console.log(f/e);
+    console.log("Done")
+}
+division(2,30)
 
-// let modulus = (g,h) =>{ // answer will be in number after round off
-//     console.log(g%h);
-//     console.log(h%g);
-//     console.log("Done")
-// }
-// modulus(2,3);
+let modulus = (g,h) =>{ // answer will be in number after round off
+    console.log(g%h);
+    console.log(h%g);
+    console.log("Done")
+}
+modulus(2,3);
 
 
 //              //**** COMPARISON_&_LOGICAL OPERATORS ******//
-//     if(4 === 5){
-//         console.log("it's equal")
-//     }else{
-//         console.log("it's not equal")
-//     }
+    if(4 === 5){
+        console.log("it's equal")
+    }else{
+        console.log("it's not equal")
+    }
 
-// let grater_equal = (age) =>{
-//     if(age == 18 || age > 18){
-//         console.log("Great! You can ride a bike")
-//     }else{
-//         console.log("Sorry! You are not able to ride a bike")
-//     }
-// }
-// grater_equal(17);
+    if(!(1==1)){
+        console.log("Right");
+    }else{
+        console.log("Wrong")
+    }
+
+let grater_equal = (age) =>{
+    if(age == 18 || age > 18){
+        console.log("Great! You can ride a bike")
+    }else{
+        console.log("Sorry! You are not able to ride a bike")
+    }
+}
+grater_equal(17);
 
 
-// let less_equal = (age) =>{
-//     if(age == 16 || age >= 18){
-//         console.log("Sorry! This Pool is only for less than 16 years old")
-//     }else{
-//         console.log("Welcome! You can bath in a Pool **Enjoy**")
-//     }
-// }
-// less_equal(23)
+let less_equal = (age) =>{
+    if(age == 16 || age >= 18){
+        console.log("Sorry! This Pool is only for less than 16 years old")
+    }else{
+        console.log("Welcome! You can bath in a Pool **Enjoy**")
+    }
+}
+less_equal(23)
+
 
 
 
